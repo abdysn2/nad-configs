288825414a508e87effa411a78a04f1c2674d708
abdysn2 <alsaffa71994@gmail.com>
2023-10-19 09:46:55 +0000

    Add an example kpt package
    Added an example kpt package that uses the
    `configure-nad` function to configure an sriov
    pod
    
    Signed-off-by: abdysn2 <alsaffa71994@gmail.com>

--- configure-nad/main.go
+++ configure-nad/main.go
@@ -1,17 +1,3 @@
-// Copyright 2022 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -23,15 +9,10 @@ import (
 
 var _ fn.Runner = &ConfigureNad{}
 
-// TODO: Change to your functionConfig "Kind" name.
 type ConfigureNad struct {
         Configs map[string]string
 } 
 
-// Run is the main function logic.
-// `items` is parsed from the STDIN "ResourceList.Items".
-// `functionConfig` is from the STDIN "ResourceList.FunctionConfig". The value has been assigned to the r attributes
-// `results` is the "ResourceList.Results" that you can write result info to.
 func (cn *ConfigureNad) Run(ctx *fn.Context, functionConfig *fn.KubeObject, items fn.KubeObjects, results *fn.Results) bool {
         if _, ok := cn.Configs["config"]; !ok {
             *results = append(*results, fn.GeneralResult("config is missing!", fn.Error))
